dba55570db3c0f9cacc957e7ae7764a4a644f6c3
Rogerio Junior <rjunior@mit.edu>
2019-12-01 23:55:47 -0500

    added sgd

--- meta_ops.py
+++ meta_ops.py
@@ -46,6 +46,63 @@ from support.omniglot_loaders import OmniglotNShot
 
 import pdb
 
+def train_sgd(db, net, device, meta_opt, epoch, log):
+    # call this before starting to train: this enables modules like dropout.
+    net.train()
+    n_train_iter = db.x_train.shape[0] // db.batchsz
+
+    for batch_idx in range(n_train_iter):
+        start_time = time.time()
+        # Sample a batch of support and query images and labels.
+        x_spt, y_spt, x_qry, y_qry = db.next()
+
+        task_num, setsz, c_, h, w = x_spt.size()
+        querysz = x_qry.size(1)
+
+        # Initialize the inner optimizer to adapt the parameters to
+        # the support set.
+        optimizer = torch.optim.SGD(net.parameters(), lr=1e-1)
+        # thus the inner optimizer will do 5 steps of SGD to get the fast weights
+
+        qry_losses = []
+        qry_accs = []
+        optimizer.zero_grad()
+
+        # so task_num is the meta batch
+        for i in range(task_num):
+            # now we are at task i: we have 32 tasks with 5 sampled classes
+            # (5-way) and 5 examples from each class (5-shot)
+
+            spt_logits = net(x_spt[i])
+            spt_loss = F.cross_entropy(spt_logits, y_spt[i])
+
+            # the query has N_way x K_query examples, in this case 5 x 15 = 75
+            qry_logits = net(x_qry[i])
+            qry_loss = F.cross_entropy(qry_logits, y_qry[i])
+            qry_losses.append(qry_loss.detach())
+            qry_acc = (qry_logits.argmax(
+                dim=1) == y_qry[i]).sum().item() / querysz
+            qry_accs.append(qry_acc)
+
+            spt_loss.backward()
+
+        optimizer.step()
+
+        qry_losses = sum(qry_losses) / task_num
+        qry_accs = 100. * sum(qry_accs) / task_num
+        i = epoch + float(batch_idx) / n_train_iter
+        iter_time = time.time() - start_time
+        if batch_idx % 4 == 0:
+            print(f'[Epoch {i:.5f}] Train Loss: {qry_losses:.5f} | Acc: {qry_accs:.5f} | Time: {iter_time:.5f}')
+
+        log.append({
+            'epoch': i,
+            'loss': qry_losses,
+            'acc': qry_accs,
+            'mode': 'train',
+            'time': time.time(),
+        })
+
 def train_maml(db, net, device, meta_opt, epoch, log):
     # call this before starting to train: this enables modules like dropout.
     net.train()
@@ -260,6 +317,48 @@ def test_maml(db, net, device, epoch, log):
     qry_accs = 100. * torch.cat(qry_accs).float().mean().item()
     print(
         f'[Epoch {epoch+1:.2f}] Test Loss: {qry_losses:.2f} | Acc: {qry_accs:.2f}'
+    )
+    log.append({
+        'epoch': epoch + 1,
+        'loss': qry_losses,
+        'acc': qry_accs,
+        'mode': 'test',
+        'time': time.time(),
+    })
+
+def test_sgd(db, net, device, epoch, log):
+    print('testing')
+    # Crucially in our testing procedure here, we do *not* fine-tune
+    # the model during testing for simplicity.
+    # Most research papers using MAML for this task do an extra
+    # stage of fine-tuning here that should be added if you are
+    # adapting this code for research.
+    net.train()
+    n_test_iter = db.x_test.shape[0] // db.batchsz
+
+    qry_losses = []
+    qry_accs = []
+
+    for batch_idx in range(n_test_iter):
+        x_spt, y_spt, x_qry, y_qry = db.next('test')
+
+
+        task_num, setsz, c_, h, w = x_spt.size()
+
+        for i in range(task_num):
+
+            # The query loss and acc induced by these parameters.
+            qry_logits = net(x_qry[i]).detach()
+            qry_loss = F.cross_entropy(
+                qry_logits, y_qry[i], reduction='none')
+            qry_losses.append(qry_loss.detach())
+            qry_acc =  (qry_logits.argmax(dim=1) == y_qry[i]).detach()
+            qry_accs.append(qry_acc)
+
+    qry_losses = torch.cat(qry_losses).mean().item()
+    qry_accs = 100. * torch.cat(qry_accs).float().mean().item()
+    print(
+        f'[Epoch {epoch+1:.5f}] Test Loss: {qry_losses:.5f} | Acc: {qry_accs:.5f}'
     )
     log.append({
         'epoch': epoch + 1,
